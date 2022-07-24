9404f4ed4fa5af55343a29df0e2c140037fa6d9d
luxamrown <luxamrownn@gmail.com>
2022-07-24 21:23:43 +0700

    Go JWT

--- main.go
+++ main.go
@@ -2,18 +2,65 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
+
+var ApplicationName = "Boeing"
+var JwtSigningMethod = jwt.SigningMethodHS256
+var JwtSignatureKey = []byte("p@ssw0rd")
+
+type MyClaims struct {
+	jwt.StandardClaims
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+}
 
 type AuthHeader struct {
 	AuthorizationHeader string `header:"Authorization"`
 }
 
 type Credential struct {
-	Username string
-	Password string
+	Username string `json:"userName"`
+	Password string `json:"userPassword"`
+}
+
+func ParseToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		method, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, fmt.Errorf("Signing method invalid")
+		} else if method != JwtSigningMethod {
+			return nil, fmt.Errorf("Signing method invalid")
+		}
+		return JwtSignatureKey, nil
+	})
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
+
+func GenerateToken(userName string, email string) (string, error) {
+	claims := MyClaims{
+		StandardClaims: jwt.StandardClaims{
+			Issuer: ApplicationName,
+		},
+		Username: userName,
+		Email:    email,
+	}
+
+	token := jwt.NewWithClaims(
+		JwtSigningMethod,
+		claims,
+	)
+	return token.SignedString(JwtSignatureKey)
 }
 
 func AuthTokenMiddleware() gin.HandlerFunc {
@@ -28,15 +75,34 @@ func AuthTokenMiddleware() gin.HandlerFunc {
 					"message": "Unauthorized",
 				})
 				c.Abort()
+				return
 			}
-
-			if h.AuthorizationHeader == "112233" {
+			tokenString := strings.Replace(h.AuthorizationHeader, "Bearer ", "", -1)
+			fmt.Println(tokenString)
+			if tokenString == "" {
+				c.JSON(401, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+				return
+			}
+			token, err := ParseToken(tokenString)
+			if err != nil {
+				c.JSON(401, gin.H{
+					"message": "Unauthorized",
+				})
+				c.Abort()
+				return
+			}
+			fmt.Println(token)
+			if token["iss"] == ApplicationName {
 				c.Next()
 			} else {
 				c.JSON(401, gin.H{
 					"message": "Unauthorized",
 				})
 				c.Abort()
+				return
 			}
 		}
 	}
@@ -56,8 +122,12 @@ func main() {
 		}
 
 		if user.Username == "test" && user.Password == "testadmin" {
+			token, err := GenerateToken(user.Username, "user@corp.com")
+			if err != nil {
+				c.AbortWithStatus(401)
+			}
 			c.JSON(200, gin.H{
-				"token": "123",
+				"token": token,
 			})
 		} else {
 			c.JSON(401, gin.H{
